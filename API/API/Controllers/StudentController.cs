a54395f0050254a15a32a6f2e586296e1e8e8aed
Jomari <addujomcpe.@gmail.com>
2018-04-11 11:58:31 +0800

    add new files

---
+++ API/API/Controllers/StudentController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+
+        private readonly BasicSchoolContext _context;
+        public StudentController(BasicSchoolContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Student
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            return _context.Student.ToArray();
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}", Name = "Get")]
+        public string GetById(int id)
+        {
+            var student = _context.Student.FirstOrDefault(s => s.StudentId == id);
+            if (student != null) return student.Name;
+            return "value";
+        }
+        
+        // Create
+        // POST: api/Student
+        [HttpPost]
+        public IActionResult Post([FromBody]Student value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Student.Add(value);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("Get", new {id = value.StudentId}, value);
+        }
+        
+        // Update
+        // PUT: api/Student/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Student value)
+        {
+            if (value == null || value.StudentId != id)
+            {
+                return BadRequest();
+            }
+
+            var student = _context.Student.FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            student.Name = value.Name;
+            student.Age = value.Age;
+            student.ClassRoomId = value.ClassRoomId;
+            student.Gender = value.Gender;
+            student.Grade = value.Grade;
+
+            _context.Student.Update(student);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Student.FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Student.Remove(student);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+
+        }
+    }
+}
---
+++ API/API/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            var connection =
+                @"Data Source=192.168.254.104\LAPTOP-VPLAC25E\SQLEXPRESS,1433;Initial Catalog=BasicSchool;User ID=sa;Password=jomari";
+            services.AddDbContext<BasicSchoolContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
